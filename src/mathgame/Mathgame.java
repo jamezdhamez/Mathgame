d4bdf97340bbb17a1709c4b7722071700b3399dd
jamestruhol <jamestruhol@fe80:0:0:0:aede:48ff:fe00:1122%7>
2020-02-20 10:12:25 +0800

    second commit

--- src/mathgame/Mathgame.java
+++ src/mathgame/Mathgame.java
@@ -25,26 +25,26 @@ public class Mathgame {
         Random rand = new Random();
         int a;
         int b;
-        int correct;
+        int correct = 0;
         int score = 0;
         int option1 = 1;
         int option2 = 2;
         int option3 = 3;
         int option4 = 4;
-            
-        
-        
-        
         if(input == 1){
             
             System.out.println("You have chosen to start a new game! ");
             System.out.println("There will be 12 math questions total, with four answers to choose from \nThe points allocate as following: \n+1 point for addition \n+2 points for subtraction \n+3 points for multiplication \n+4 points for division");
             System.out.println("Are you ready for your questions? \n1.Yes \n2.No (Quit the game)");
             input = scanner.nextInt();
+            a = rand.nextInt(51);
+            b = rand.nextInt(51);
+           
             if(input == 1){
-                System.out.println("First Question! "); //Main math game function here
-                a = rand.nextInt(51);
-                b = rand.nextInt(51);
+                
+                for (int i = 0; i <12; i++){
+                    if (i < 3){ //addition questions
+                        System.out.println("Addition Questions!");
                 System.out.println("What is " + a + "+" + b + "?");
                 
                 System.out.println(option1 + "." + (a+b) );
@@ -55,33 +55,88 @@ public class Mathgame {
                 input = scanner.nextInt();
                 if(input == correct ){
                     System.out.println("Correct!");
+                    score = score +1;
                     
                 }
                 else{
                     System.out.println("Incorrect!");
+                    break;
                     //add scoring system here
                 }
-                
-                
-                
-                
-                
             }
-            else if(input == 2){
-                System.out.println("You have chosen to quit the game, see you next time!");
+            if (i >= 3 && i < 7){       //subtraction questions
+                System.out.println("Subtraction Questions!");
+                a = rand.nextInt(51);
+                b = rand.nextInt(51);
+                System.out.println("What is " + a + "-" + b + "?");
+                
+                System.out.println(option1 + "." + (a+b) );
+                System.out.println(option2 + "." + (a-b) );
+                System.out.println(option3 + "." + (a*b) );
+                System.out.println(option4 + "." + (a/b) );
+                correct = option2;
+                input = scanner.nextInt();
+                    
+                if(input == correct ){
+                    System.out.println("Correct!");
+			score = score + 2;
+		}
+                else{
+                    System.out.println("Incorrect!");
+                    break;
             }
-            else{
-                System.out.println("Incorrect entry, please try again");  //find a way to make this not repeat itself
+
+		
+	}
+
+	if (i >= 7 && i < 9){   //multiplication questions
+            System.out.println("Multiplication Questions!");
+                a = rand.nextInt(51);
+                b = rand.nextInt(51);
+                System.out.println("What is " + a + "*" + b + "?");
+                
+                System.out.println(option1 + "." + (a+b) );
+                System.out.println(option2 + "." + (a-b) );
+                System.out.println(option3 + "." + (a*b) );
+                System.out.println(option4 + "." + (a/b) );
+                correct = option3;
+                input = scanner.nextInt();
+                if(input == correct){
+                    System.out.println("Correct!");
+			score = score + 3;
+                        
+		}
+                else{
+                    System.out.println("Incorrect!");
+                    break;
+                }
             }
-        } 
-        else if(input ==2){
+		
+		
+                if (i >= 9 && i < 12){
+
+		//do division questions
+		//in terms of creating a division question, think about division in 
+//terms of multiplication. 3*4 = 12 but 12/4 = 3.
+		input = scanner.nextInt();
+		if(input == correct){
+                    System.out.println("Correct!");
+			score = score + 4;
+		}
+	}
+                
+		
+	}
+		
+	}
+        if(input ==2){
             System.out.println("Highest Score:"); //Add Highest Score Here 
             System.out.println("Lowest Score:"); //Add Lowest Score Here 
             System.out.println("All scores"); //Add all scores here
             System.out.println("Would you like to play the game? \nPress '1' to play or '2' to quit");
             input = scanner.nextInt();
             if(input ==1 ){
-                //Loop back to main math game function
+                
             }
             else if(input ==2){
                 System.out.println("You have chosen to quit the game, see you next time!");
@@ -93,11 +148,26 @@ public class Mathgame {
     }
         else if(input == 3){
             System.out.println("You have chosen to quit the game, see you next time!");
-            
-        }
+        
+        
+        
+        
+
+	}
         else {
             System.out.println("Invalid entry, please try again");
         }
+
+	
+}
+         else if(input == 2){
+                System.out.println("You have chosen to quit the game, see you next time!");
+            }
+            else{
+                System.out.println("Incorrect entry, please try again");  //find a way to make this not repeat itself
+            }
+        } 
+        
             
             
         }
@@ -105,7 +175,12 @@ public class Mathgame {
        
         
         
-    }
+    
+                
+           
+                
+            
+           
     
 
 
